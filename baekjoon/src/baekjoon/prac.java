39b77c1e57080031d1b41dea39dacaa5911701f6
hyeonjin <hyoenjin0103@naver.com>
2020-12-22 23:25:07 +0900

    백준연동시작

---
+++ baekjoon/src/baekjoon/prac.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class prac {
+	public static void main(String arg[]) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		System.out.println("d");
+		for(int i = m; i<=n; i++) {
+			//이제 올려야지
+			//올리기
+		}
+	}
+}
